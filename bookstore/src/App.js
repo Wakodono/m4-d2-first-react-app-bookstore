f10be223bda53f78701035e479b447c75727cd08
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-09-28 15:14:46 +0100

    Create react app
    Installed all necessary dependencies
    
    Navbar Component
    
    Footer Component

---
+++ bookstore/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNav from './components/MyNavbar';
+import MyFooter from './components/MyFooter';
+
+function App() {
+  return (
+    <div>
+      <MyNav />
+      <MyFooter />
+    </div>
+  );
+}
+
+export default App;
---
+++ bookstore/src/components/MyFooter.jsx
@@ -0,0 +1,7 @@
+const MyFooter = () => (
+    <footer className="d-flex bg-dark fixed-bottom">
+      <p className="text-white m-auto">Property of Wako - Copyright 2021 </p>
+    </footer>
+ )
+ 
+ export default MyFooter
